3e78ab47362c25e9971b947b59f35b506feb2561
Soohyun-Kim <imsh0314@sch.ac.kr>
2022-05-25 17:40:15 +0900

    Update mission1-2.js

--- 11-제이쿼리의 소개/mission1-2.js
+++ 11-제이쿼리의 소개/mission1-2.js
@@ -1,4 +1,7 @@
+/* 버튼 클릭시 상자의 크기를 600 * 600, 왼쪽을 기준으로 300px 만큼 이동하는 jQuery 코드를 작성해 보세요. */
+
 $(function() { $('button').click(function() {
+  
   $('#move').width('600px'); 
   $('#move').height('600px'); 
   $('#move').css('left', '300px'); 
